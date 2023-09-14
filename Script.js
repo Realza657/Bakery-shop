5b124ab5a41065f2c9256081d85fda9c434cd8a5
Realza657 <145000225+Realza657@users.noreply.github.com>
2023-09-14 20:07:43 +1000

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const firstLi = document.querySelector('li');
-const map = document.querySelector('iframe');
-
-firstLi.addEventListener("click", (el) => {
-    map.scrollIntoView({behavior: "smooth", block: "center"});
-});
